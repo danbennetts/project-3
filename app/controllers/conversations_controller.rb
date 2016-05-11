06f31de51eb567b932dd87f30e341a59c30265d5
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-05-11 17:51:25 +1000

    added some more methods to controllers that i have created for messages and conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,6 +3,17 @@ class ConversationsController < ApplicationController
 
     def index
       @users = User.all
+      @conversations = Conversation.involving(current_user)
+    end
+
+    def create
+      if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+        @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+      else
+        @conversation = Conversation.create(conversation_params)
+      end
+
+      redirect_to conversation_messages_path(@conversation)
     end
 
   private
